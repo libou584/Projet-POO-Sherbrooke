1ff49d704545d45ee9b2f7b9e19ad58456629e4d
lilian <li4bou@gmail.com>
2025-03-19 09:38:27 -0400

    feat : basic day off booking

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from urllib.parse import quote as url_quote
+from flask_wtf import FlaskForm
+from wtforms import DateField, SubmitField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+booked_days = []
+
+
+class BookingForm(FlaskForm):
+    date = DateField('Select a Date', format='%Y-%m-%d', validators=[DataRequired()])
+    submit = SubmitField('Book Day Off')
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    
-
-    return render_template('pages/index.html')
+    form = BookingForm()
+    if form.validate_on_submit():
+        booked_days.append(form.date.data.strftime('%Y-%m-%d'))
+        return redirect(url_for('index'))
+    return render_template("pages/index.html", form=form, booked_days=booked_days)
 
 if __name__ == '__main__':
     app.run(debug=True)
