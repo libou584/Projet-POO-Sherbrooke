0ac3a40aed8aff84da8902898c936104da4ff0b2
lilian <li4bou@gmail.com>
2025-03-23 13:09:22 -0400

    feat : added booked days in Application (not its final position)

--- models/Application.py
+++ models/Application.py
@@ -1,10 +1,26 @@
 class Application:
     __instance = None
 
+    def __new_app(self):
+        self.__user_logged_in = None
+        self.__database_handler = None
+        self.__booked_days: list[str] = []
+
     def __new__(cls):
         if cls.__instance is None:
             instance = super().__new__(cls)
-            instance.__user_logged_in = None
-            instance.__database_handler = None
+            instance.__new_app()
             cls.__instance = instance
         return cls.__instance
+    
+    @property
+    def booked_days(self):
+        return self.__booked_days
+    
+    def book_day(self, date: str):
+        if date not in self.__booked_days:
+            self.__booked_days.append(date)
+            self.sort_dates()
+    
+    def sort_dates(self):
+        self.__booked_days.sort(key=lambda x: tuple(map(int, x.split('-'))))
