0f95801a534921655196a97335f9c030d61b62a9
lilian <li4bou@gmail.com>
2025-03-23 14:05:22 -0400

    feat: Added Employee class

---
+++ models/Employee.py
@@ -0,0 +1,20 @@
+from models.User import User
+
+
+class Employee(User):
+
+    def __init__(self, id: int, first_name: str, last_name: str, email: str):
+        super().__init__(id, first_name, last_name, email)
+        self.__booked_days = []
+
+    @property
+    def booked_days(self):
+        return self.__booked_days
+    
+    def book_day(self, date: str):
+        if date not in self.__booked_days:
+            self.__booked_days.append(date)
+            self.sort_dates()
+    
+    def sort_dates(self):
+        self.__booked_days.sort(key=lambda x: tuple(map(int, x.split('-'))))
