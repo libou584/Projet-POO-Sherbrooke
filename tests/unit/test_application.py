d1586a3186c3f4201fc4b837e3a02dec5e90f1fc
lilian <li4bou@gmail.com>
2025-03-23 13:10:36 -0400

    test: updated tests for Application

--- tests/unit/test_application.py
+++ tests/unit/test_application.py
@@ -7,3 +7,15 @@ def test_application_instance():
     app2 = Application()
     assert app1 == app2
     assert app1 is app2
+
+
+def test_book_day():
+    app = Application()
+    app.book_day('2021-01-04')
+    assert app.booked_days == ['2021-01-04']
+    app.book_day('2021-01-05')
+    assert app.booked_days == ['2021-01-04', '2021-01-05']
+    app.book_day('2021-01-04')
+    assert app.booked_days == ['2021-01-04', '2021-01-05']
+    app.book_day('2021-01-03')
+    assert app.booked_days == ['2021-01-03', '2021-01-04', '2021-01-05']
