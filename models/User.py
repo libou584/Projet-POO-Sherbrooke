2fecdb8a546a9587e7b4e387da35ce7b51aaf6d8
lilian <li4bou@gmail.com>
2025-03-23 14:04:56 -0400

    feat: added User abstract class

---
+++ models/User.py
@@ -0,0 +1,25 @@
+from abc import ABC as AbstractClass
+
+
+class User(AbstractClass):
+
+    def __init__(self, id: int, first_name: str, last_name: str, email: str):
+        self.__id = id
+        self.__first_name = first_name
+        self.__last_name = last_name
+        self.__email = email
+
+    def __str__(self):
+        return f"{self.__first_name} {self.__last_name} <{self.__email}>"
+    
+    @property
+    def first_name(self):
+        return self.__first_name
+    
+    @property
+    def last_name(self):
+        return self.__last_name
+    
+    @property
+    def email(self):
+        return self.__email
