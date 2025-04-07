92dfe0465f20e6be233a0172404989740eb4d565
lilian <li4bou@gmail.com>
2025-04-07 11:28:00 -0400

    feat: added day off approval in index page

--- app.py
+++ app.py
@@ -28,6 +28,7 @@ def index():
         date = form.date.data.strftime('%Y-%m-%d')
         res = application.repository.add_booked_day(application.user.id, date)
         if res:
+            application.day_off_approval_strategy.approve(application.user.id, date)
             application.user.refresh()
         return redirect(url_for('index'))
     return render_template("pages/index.html", user = application.user, form = form, booked_days = application.user.booked_days)
