309ee7d8767531c4a7755e7b646bd9c64c4e50b0
lilian <li4bou@gmail.com>
2025-04-07 11:25:58 -0400

    change: day off approval strategy in Application

--- models/Application.py
+++ models/Application.py
@@ -1,4 +1,5 @@
 from models.Repository import Repository
+from models.DayOffApproval.HRToApproveStrategy import HRToApproveStategy
 
 
 class Application:
@@ -8,6 +9,7 @@ class Application:
     def __new_app(self):
         self.__logged_in_user = None
         self.__repository = Repository()
+        self.__day_off_approval_strategy = HRToApproveStategy()
 
     def __new__(cls):
         if cls.__instance is None:
@@ -24,9 +26,17 @@ class Application:
     def repository(self):
         return self.__repository
     
+    @property
+    def day_off_approval_strategy(self):
+        return self.__day_off_approval_strategy
+    
     @repository.setter
     def repository(self, repository):
         self.__repository = repository
+
+    @day_off_approval_strategy.setter
+    def day_off_approval_strategy(self, strategy):
+        self.__day_off_approval_strategy = strategy
     
     def login(self, user):
         self.__logged_in_user = user
